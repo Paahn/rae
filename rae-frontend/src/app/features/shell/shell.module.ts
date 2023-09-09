74a8fe046612db0607bf9fb3d1236fb8fdaa8ca1
Paahn <paahndev@gmail.com>
2023-09-09 17:47:35 -0400

    dashboard component stub

--- rae-frontend/src/app/features/shell/shell.module.ts
+++ rae-frontend/src/app/features/shell/shell.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShellRoutingModule } from './shell-routing.module';
+import { PageDashboardComponent } from './page-dashboard/page-dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageDashboardComponent
+  ],
   imports: [
     CommonModule,
     ShellRoutingModule
