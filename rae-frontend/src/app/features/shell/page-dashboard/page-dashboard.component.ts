e67c5c9bbff362717fe3694f0129453ea9dfc330
Paahn <paahndev@gmail.com>
2023-09-10 22:35:01 -0400

    added icons from fontawesome

--- rae-frontend/src/app/features/shell/page-dashboard/page-dashboard.component.ts
+++ rae-frontend/src/app/features/shell/page-dashboard/page-dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-
+import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-page-dashboard',
   templateUrl: './page-dashboard.component.html',
   styleUrls: ['./page-dashboard.component.scss'],
 })
-export class PageDashboardComponent {}
+export class PageDashboardComponent {
+  public faLinkedin = faLinkedin;
+  public faFacebook = faFacebook;
+}
