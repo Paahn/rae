4d0902c7d576733aad6a16c15a606c90cde78a03
Paahn <paahndev@gmail.com>
2023-09-12 12:33:35 -0400

    facebook url variable

--- rae-frontend/src/app/features/shell/page-dashboard/page-dashboard.component.ts
+++ rae-frontend/src/app/features/shell/page-dashboard/page-dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { APP_CONFIG, AppConfig } from 'src/app/app.config';
 @Component({
   selector: 'app-page-dashboard',
   templateUrl: './page-dashboard.component.html',
@@ -8,4 +9,9 @@ import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 export class PageDashboardComponent {
   public faLinkedin = faLinkedin;
   public faFacebook = faFacebook;
+  public raeFacebookUrl: string;
+
+  public constructor(@Inject(APP_CONFIG) private config: AppConfig) {
+    this.raeFacebookUrl = this.config.urls.raeFacebook;
+  }
 }
---
+++ rae-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { AppEnvironment } from './environment.model';
+
+export const environment: AppEnvironment = {
+  urls: {
+    raeFacebook: `https://www.facebook.com/SocialApeRAE/`,
+  },
+};
