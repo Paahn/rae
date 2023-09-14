5e2ef8b83f90d3cbd77f6555f392a1c085bbbade
Paahn <paahndev@gmail.com>
2023-09-14 00:42:55 -0400

    dashboard menu

--- rae-frontend/src/scss/material.module.ts
+++ rae-frontend/src/scss/material.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
-const modules = [MatButtonModule, MatDividerModule, MatListModule];
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules = [
+  MatButtonModule,
+  MatDividerModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule,
+];
 
 @NgModule({
   imports: [modules],
