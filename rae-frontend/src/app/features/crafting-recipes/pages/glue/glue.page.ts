ce80480c9e50d514c8d560f1e28de0fc17e9c98f
Paahn <paahndev@gmail.com>
2023-09-16 00:20:46 -0400

    glue child

---
+++ rae-frontend/src/app/features/crafting-recipes/pages/glue/glue.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-glue',
+  templateUrl: './glue.page.html',
+  styleUrls: ['./glue.page.scss']
+})
+export class GluePage {
+
+}
