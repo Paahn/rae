688d206d7f52c77fc878599c77d057852fea6278
Paahn <paahndev@gmail.com>
2023-09-16 01:30:51 -0400

    import routing module

--- rae-frontend/src/app/features/about/about.module.ts
+++ rae-frontend/src/app/features/about/about.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AboutPage } from './about.page';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AboutRoutingModule } from './about-routing.module';
 
 @NgModule({
   declarations: [AboutPage],
-  imports: [SharedModule],
+  imports: [AboutRoutingModule, SharedModule],
 })
 export class AboutModule {}
