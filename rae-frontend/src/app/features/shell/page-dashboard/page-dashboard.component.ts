7f620037ee67502211f2ce669a53eb37ac4a484e
Paahn <paahndev@gmail.com>
2023-10-31 11:27:28 -0400

    display social links in dashboard

--- rae-frontend/src/app/features/shell/page-dashboard/page-dashboard.component.ts
+++ rae-frontend/src/app/features/shell/page-dashboard/page-dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, Inject, ViewChild } from '@angular/core';
 import { MatSidenav, MatDrawerMode } from '@angular/material/sidenav';
-import { faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {
+  faFacebook,
+  faLinkedin,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 import { APP_CONFIG, AppConfig } from 'src/app/app.config';
 @Component({
   selector: 'app-page-dashboard',
@@ -12,11 +16,16 @@ export class PageDashboardComponent {
 
   public faLinkedin = faLinkedin;
   public faFacebook = faFacebook;
+  public faInstragram = faInstagram;
   public raeFacebookUrl: string;
+  public raeLinkedinUrl: string;
+  public raeInstagramUrl: string;
   public sidenavMode: MatDrawerMode = 'over';
 
   public constructor(@Inject(APP_CONFIG) private config: AppConfig) {
     this.raeFacebookUrl = this.config.urls.raeFacebook;
+    this.raeLinkedinUrl = this.config.urls.raeLinkedin;
+    this.raeInstagramUrl = this.config.urls.raeInstagram;
   }
 
   public onMenuItemSelect(): void {
