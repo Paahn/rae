ba07b73f5aeb576f47526b79f79921c17478aa2e
Paahn <paahndev@gmail.com>
2023-10-31 11:27:05 -0400

    new social link values

--- rae-frontend/src/environments/environment.prod.ts
+++ rae-frontend/src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ import { AppEnvironment } from './environment.model';
 export const environment: AppEnvironment = {
   urls: {
     raeFacebook: `https://www.facebook.com/SocialApeRAE/`,
+    raeLinkedin: `https://www.linkedin.com/company/renegade-ape-exhibits/`,
+    raeInstagram: `https://www.instagram.com/_socialape_/`,
   },
 };
