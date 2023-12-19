45d4d5183f64014866afde8abe1f840ebc7c21d0
Paahn <paahndev@gmail.com>
2023-12-19 18:21:40 -0500

    unneeded

--- rae-frontend/src/app/app.module.ts
+++ rae-frontend/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { APP_CONFIG, APP_DI_CONFIG } from './app.config';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, CoreModule, SharedModule],
+  imports: [AppRoutingModule, CoreModule],
   providers: [{ provide: APP_CONFIG, useValue: APP_DI_CONFIG }],
   bootstrap: [AppComponent],
 })
