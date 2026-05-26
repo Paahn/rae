3233d95335cf551e410cbf417316bbed62583554
Paahn <paahndev@gmail.com>
2026-05-26 17:30:53 -0400

    Revert "wip"
    This reverts commit 80efbefe788bfd038da888c34b338a7b980a6612.

--- rae-frontend/src/app/features/exhibits/exhibits.page.ts
+++ rae-frontend/src/app/features/exhibits/exhibits.page.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { contentContainerSelector } from 'src/app/app.constants';
-import { UtilsService } from 'src/app/core/services/utils.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-exhibits',
   templateUrl: './exhibits.page.html',
-  styleUrls: ['./exhibits.page.scss'],
+  styleUrls: ['./exhibits.page.scss']
 })
-export class ExhibitsPage implements OnInit {
-  public constructor(private utilsService: UtilsService) {}
+export class ExhibitsPage {
 
-  public ngOnInit(): void {
-    this.utilsService.scrollTop(contentContainerSelector);
-  }
 }
